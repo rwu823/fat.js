440a624782f230f96c5a5fd6fec430678bef6612
rwu823 <rocky823@gmail.com>
2013-10-19 21:11:06 +0800

    update v0.0.1

---
+++ fat.js
@@ -0,0 +1,88 @@
+!function(){
+  'use strict'  
+
+  var isType = function(o){
+        return /\[object (\w+)\]/.test({}.toString.call(o)) && RegExp.$1
+      },
+
+      isObject = function(o){ return isType(o) === 'Object'},
+      
+      trim = function(str){
+        return str.replace(/^\s+|\s+$/g, '')
+      },
+      extend = function(a, b, isExtendObj){
+        for(var bKey in b){
+          if(isObject(a[bKey]) && isObject(b[bKey]) && isExtendObj){
+            extend(a[bKey], b[bKey], true)
+          }else{
+            a[bKey] = b[bKey]
+          }
+        }
+        return a
+      },
+      setStyle = function(_el, o){
+        for(var k in o){
+          _el.style[k] = o[k]
+        }
+      }
+
+  var Fat = {
+        version : '0.0.1'
+      }
+
+  Fat.settings = {
+        width: 200,
+        height: 200,
+        fontSize: 16,
+        color: '#999',
+        bg: '#ddd'
+      }
+
+  Fat.run = function(fats){
+    var set = Fat.settings
+        
+    for(var i = 0, fat; fat = fats[i]; i++){
+      var props = fat.attributes[0].name.split('&'),
+          wh = props[0].toLowerCase().split('x'),
+          
+          w  = (wh[0] || set.width) + 'px',
+          h = (wh[1] || set.height) + 'px',
+          bg = props[1] || set.bg,
+          color = props[2] || set.color,
+          txt = props[0] + ' ' + trim(fat.innerText)
+      
+      setStyle(fat, {
+        display: 'inline-block',
+        textAlign: 'center',
+        fontSize: set.fontSize + 'px',
+        lineHeight: h,
+        width: w,
+        height: h,
+        color: color,
+        backgroundColor: bg        
+      })   
+      
+      fat.innerText = txt
+      fat.setAttribute('skip', '1')
+    }
+
+    return Fat
+  }
+
+  Fat.setDefaut = function(op){
+    Fat.settings = extend(op, Fat.settings)
+    return Fat
+  }
+
+  Fat.init = function(){
+    Fat.run(document.querySelectorAll('fat'))
+    return Fat
+  }
+
+  Fat.update = function(){
+    Fat.run(document.querySelectorAll('fat:not([skip="1"])'))
+    return Fat
+  }
+
+  window.FatPlaceHolder = Fat
+}() 
